b118fe8d040a5ff0e244e027646a4e208352faf4
Shirali Ahat <shirali.sdet@gmail.com>
2022-10-30 21:39:47 -0700

    create firstTestCase

---
+++ src/test/java/com/globalRelay/step_definitions/firstTestCase.java
@@ -0,0 +1,9 @@
+package com.globalRelay.step_definitions;
+
+public class firstTestCase {
+    public static void main(String[] args) {
+
+
+        System.out.println("first commit update!!!");
+    }
+}
