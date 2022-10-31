1e6b3bb457f489bad29fd2413e0a9e9063b58dcd
Shirali Ahat <shirali.sdet@gmail.com>
2022-10-30 21:54:19 -0700

    new update in third test case

--- src/test/java/com/globalRelay/step_definitions/testCase3.java
+++ src/test/java/com/globalRelay/step_definitions/testCase3.java
@@ -3,5 +3,7 @@ package com.globalRelay.step_definitions;
 public class testCase3 {
     public static void main(String[] args) {
         System.out.println("create 3 test case");
+
+        System.out.println("update in third test case");
     }
 }
