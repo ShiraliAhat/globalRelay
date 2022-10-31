f202261839dfe62d72e4a04fd8ca808e6af419b0
Shirali Ahat <shirali.sdet@gmail.com>
2022-10-30 21:56:06 -0700

    create new class here

---
+++ src/test/java/com/globalRelay/step_definitions/createNewClassIn0002.java
@@ -0,0 +1,8 @@
+package com.globalRelay.step_definitions;
+
+public class createNewClassIn0002 {
+
+    public static void main(String[] args) {
+        System.out.println("create new class here");
+    }
+}
